d3d4f7a204d8c78a5e024448bd006be8a65d89de
Andrew Loucky <andrew.loucky@gmail.com>
2016-04-29 17:35:07 -0400

    ex 6 EC, toying with hex and oct

--- Makefile
+++ Makefile
@@ -6,6 +6,7 @@ clean:
 	rm -f ex4
 	rm -f ex5
 	rm -f ex6
+	rm -f ex6_ec
 
 all:
 	make ex1
@@ -13,3 +14,4 @@ all:
 	make ex4
 	make ex5
 	make ex6
+	make ex6_ec
---
+++ ex6_ec.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	// extra credit
+	unsigned int hex_1 = 1;
+	unsigned int hex_2 = 2;
+	unsigned int hex_3 = 3;
+	unsigned int hex_4 = 4;
+	unsigned int hex_5 = 5;
+	unsigned int hex_6 = 6;
+	unsigned int hex_7 = 7;
+	unsigned int hex_8 = 8;
+	unsigned int hex_9 = 9;
+	unsigned int hex_10 = 10;
+	unsigned int hex_11 = 11;
+	unsigned int hex_12 = 12;
+	unsigned int hex_13 = 13;
+	unsigned int hex_14 = 14;
+	unsigned int hex_15 = 15;
+	unsigned int hex_16 = 16;
+
+	unsigned int oct_1 = 1;
+	unsigned int oct_2 = 2;
+	unsigned int oct_3 = 3;
+	unsigned int oct_4 = 4;
+	unsigned int oct_5 = 5;
+	unsigned int oct_6 = 6;
+	unsigned int oct_7 = 7;
+	unsigned int oct_8 = 8;
+	unsigned int oct_9 = 9;
+	unsigned int oct_10 = 10;
+	unsigned int oct_11 = 11;
+	unsigned int oct_12 = 12;
+	unsigned int oct_13 = 13;
+	unsigned int oct_14 = 14;
+	unsigned int oct_15 = 15;
+	unsigned int oct_16 = 16;
+
+	printf("You are a HEX: %o.\n", hex_1);
+	printf("You are a HEX: %o.\n", hex_2);
+	printf("You are a HEX: %o.\n", hex_3);
+	printf("You are a HEX: %o.\n", hex_4);
+	printf("You are a HEX: %o.\n", hex_5);
+	printf("You are a HEX: %o.\n", hex_6);
+	printf("You are a HEX: %o.\n", hex_7);
+	printf("You are a HEX: %o.\n", hex_8);
+	printf("You are a HEX: %o.\n", hex_9);
+	printf("You are a HEX: %o.\n", hex_10);
+	printf("You are a HEX: %o.\n", hex_11);
+	printf("You are a HEX: %o.\n", hex_12);
+	printf("You are a HEX: %o.\n", hex_13);
+	printf("You are a HEX: %o.\n", hex_14);
+	printf("You are a HEX: %o.\n", hex_15);
+	printf("You are a HEX: %o.\n", hex_16);
+
+	printf("You are a OCT: %o.\n", oct_1);
+	printf("You are a OCT: %o.\n", oct_2);
+	printf("You are a OCT: %o.\n", oct_3);
+	printf("You are a OCT: %o.\n", oct_4);
+	printf("You are a OCT: %o.\n", oct_5);
+	printf("You are a OCT: %o.\n", oct_6);
+	printf("You are a OCT: %o.\n", oct_7);
+	printf("You are a OCT: %o.\n", oct_8);
+	printf("You are a OCT: %o.\n", oct_9);
+	printf("You are a OCT: %o.\n", oct_10);
+	printf("You are a OCT: %o.\n", oct_11);
+	printf("You are a OCT: %o.\n", oct_12);
+	printf("You are a OCT: %o.\n", oct_13);
+	printf("You are a OCT: %o.\n", oct_14);
+	printf("You are a OCT: %o.\n", oct_15);
+	printf("You are a OCT: %o.\n", oct_16);
+
+	return 0;
+}
